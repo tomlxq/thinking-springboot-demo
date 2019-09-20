3911a0094f80fff1b85139202e36d3600dc66559
tomlxq <21429503@qq.com>
2019-09-20 23:11:16 +0800

    add @SpringBootApplication

--- src/main/java/thinking/springboot/com/App.java
+++ src/main/java/thinking/springboot/com/App.java
@@ -1,13 +1,22 @@
 package thinking.springboot.com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@RestController
+@SpringBootApplication
+public class App {
+    @RequestMapping
+    public String hello() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
