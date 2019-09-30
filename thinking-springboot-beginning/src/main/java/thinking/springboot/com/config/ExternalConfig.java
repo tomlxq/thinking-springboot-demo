90df981f033c89c2f89004bfbc331ec5def80f25
tomlxq <21429503@qq.com>
2019-09-30 12:26:13 +0800

    add spring boot think

--- thinking-springboot-beginning/src/main/java/thinking/springboot/com/config/ExternalConfig.java
+++ thinking-springboot-beginning/src/main/java/thinking/springboot/com/config/ExternalConfig.java
@@ -1,6 +1,7 @@
 package thinking.springboot.com.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -21,6 +22,7 @@ public class ExternalConfig {
     @Value("${spring.application.name}")
     private String name;
     @Autowired
+    @Qualifier
     private Environment env;
     @Autowired
     private ExternalProperties properties;
